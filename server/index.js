fc37db68a934664ac20ffa73b6d4b32f70158b52
Ritesh Borikar <riteshborikar1010@gmail.com>
2023-09-05 23:15:37 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ var express = require('express')
 var Mobile = require('./MobileModel')
 var cors = require('cors')
 const MobileModel = require('./MobileModel')
+const user = require('./Admin')
 var app = express()
 app.use(express.json())
 app.use(cors())
@@ -50,12 +51,32 @@ app.put('/putdata', async (req, res) => {
     try {
         const re = await MobileModel.updateOne({ _id: data._id }, {
             $set: {
-                Total : data.Total,
-                Avl : data.Avl
+                Total: data.Total,
+                Avl: data.Avl
             }
         })
-        if(re){
-            res.send({'message':'success',data})
+        if (re) {
+            res.send({ 'message': 'success', data })
+        }
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+
+app.post('/user', async (req, res) => {
+    d = req.body
+    try {
+        console.log(d)
+        const data = await user.findOne({
+            $and: [{ username:d.username },
+            { password:d.password }]
+        })
+        if (data) {
+            res.json({ data, message: "success" })
+        }
+        else {
+            res.json({ message: "not" })
         }
     } catch (err) {
         console.log(err)
